672ce9b67514ed94da3e8dfd1b37fbb37fbdea5c
Zero <jgm262@naver.com>
2024-02-26 15:14:28 +0900

    Bootstrap init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+
+const data = [
+  {
+    id: 1,
+    name: '테스트1',
+    contents: '테스트입니다.'
+  },
+  {
+    id: 2,
+    name: '테스트2',
+    contents: '테스트입니다'
+  },
+  {
+    id: 3,
+    name: '테스트3',
+    contents: '테스트입니다'
+  },
+]
+
+const Body = Accordion;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {
+        data.map((e, i) => (
+          <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey={i}>
+              <Card>
+                <Accordion.Header>
+                  <Card.Header>
+                  {e.name}
+                  </Card.Header>
+                </Accordion.Header>
+              </Card>
+
+              <Body>
+                <Card.Body>
+                  {e.contents}
+                </Card.Body>
+              </Body>
+            </Accordion.Item>
+        </Accordion>
+        ))
+      }
     </div>
   );
 }
---
+++ src/EventByBootstrap.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+const EventByBootstrap = () => {
+    return (
+        <article>
+            <div>
+                
+            </div>
+        </article>
+    )
+}
+
+export default EventByBootstrap;
